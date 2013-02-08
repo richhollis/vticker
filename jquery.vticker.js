8c7394d62232d22c5534fc33bebaf1e72978f2ff
Rich Hollis <richhollis@gmail.com>
2013-02-08 11:33:12 +0000

    Allow multiple independent vtickers on same page

--- jquery.vticker.js
+++ jquery.vticker.js
@@ -1,142 +1,174 @@
 /*
-* vertical news ticker
-* Tadas Juozapaitis ( kasp3rito [eta] gmail (dot) com )
-* http://www.jugbit.com/jquery-vticker-vertical-news-ticker/
-*/
-/* 
-	Modified by Richard Hollis 
+  Vertical News Ticker
+
+  Original by: Tadas Juozapaitis ( kasp3rito [eta] gmail (dot) com )
+               http://www.jugbit.com/jquery-vticker-vertical-news-ticker/
+
+  Modified by: Richard Hollis @richhollis - richhollis.co.uk
 */
 
 (function($){
-	var defaults = {
-		speed: 700,
-		pause: 4000,
-		showItems: 3,
-		mousePause: true,
-		isPaused: false,
-		height: 0
-	};
-	var methods = {
-		options: null,
-		maxHeight: 0,
-		obj: null,
-		animating: false,
-		init: function(options) {
-			var options = methods.options = $.extend(defaults, options);
-			var obj = methods.obj = $(this);
-			var maxHeight = 0;
 
-			obj.css({overflow: 'hidden', position: 'relative'})
-				.children('ul').css({position: 'absolute', margin: 0, padding: 0})
-				.children('li').css({margin: 0, padding: 0});
+  var defaults = {
+    speed: 700,
+    pause: 4000,
+    showItems: 1,
+    mousePause: true,
+    height: 0,
+    animate: true
+  };
 
-			if(options.height == 0)
-			{
-				obj.children('ul').children('li').each(function(){
-					if($(this).height() > maxHeight)
-					{
-						methods.maxHeight = $(this).height();
-					}
-				});
+  var internal = { 
 
-				obj.children('ul').children('li').each(function(){
-					$(this).height(maxHeight);
-				});
+    moveUp: function(state, attribs) {    
+      var height = state.itemHeight;
+      var options = state.options;
+      var el = state.element;
+      var obj = el.children('ul');
+      
+      var clone = obj.children('li:first').clone(true);
+      
+      if(options.height > 0)
+      {
+        height = obj.children('li:first').height();
+      }
 
-				obj.height(maxHeight * options.showItems);
-			}
-			else
-			{
-				obj.height(options.height);
-			}
-			
-			var interval = setInterval(function(){ 
-				methods.nextUsePause();
-			}, options.pause);
-			
-			if(options.mousePause)
-			{
-				obj.bind("mouseenter",function(){
-					methods.pause(true);
-				}).bind("mouseleave",function(){
-					methods.pause(false);
-				});
-			}
-		},
-		pause: function(state) {
-			methods.options.isPaused = state;
-		},
-		moveUp: function(obj2, height, options, moveOptions){		
-			var obj = obj2.children('ul');
-			
-			var clone = obj.children('li:first').clone(true);
-			
-			if(options.height > 0)
-			{
-				height = obj.children('li:first').height();
-			}
+      if(attribs && attribs.animate) {
+        state.animating = true;
+        obj.animate({top: '-=' + height + 'px'}, options.speed, function() {
+            $(obj).children('li:first').remove();
+            $(obj).css('top', '0px');
+            state.animating = false;
+          });
+      } else {
+        obj.children('li:first').remove();
+        obj.css('top', '0px');
+      }
 
-			if(moveOptions && moveOptions.animate) {
-				methods.animating = true;
-				obj.animate({top: '-=' + height + 'px'}, options.speed, function() {
-						$(this).children('li:first').remove();
-						$(this).css('top', '0px');
-						methods.animating = false;
-					});
-			} else {
-				obj.children('li:first').remove();
-				obj.css('top', '0px');
-			}
+      clone.appendTo(obj);
+    },
 
-			clone.appendTo(obj);
-		},
-		moveDown: function(obj2, height, options, moveOptions){
-			var obj = obj2.children('ul');
-			
-			var clone = obj.children('li:last').clone(true);
-			
-			if(options.height > 0)
-			{
-				height = obj.children('li:first').height();
-			}
-			
-			obj.css('top', '-' + height + 'px')
-				.prepend(clone);
-			
-			if(moveOptions && moveOptions.animate) {
-				if(methods.animating) return;
-				methods.animating = true;
-				obj.animate({top: 0}, options.speed, function() {
-					$(this).children('li:last').remove();
-					methods.animating = false;
-				});
-			} else {
-				obj.children('li:last').remove();
-				obj.css('top', '0px');
-			}
-		},
-		nextUsePause: function() {
-			if(methods.options.isPaused) return;
-			methods.next({animate: true});
-		},
-		next: function(options) { 
-			if(methods.animating) return false;
-			methods.moveUp(methods.obj, methods.maxHeight, methods.options, options); 
-		},
-		prev: function(options) {
-			if(methods.animating) return false;
-			methods.moveDown(methods.obj, methods.maxHeight, methods.options, options); 
-		}
-	};
+    moveDown: function(state, attribs){
+      var height = state.itemHeight;
+      var options = state.options;
+      var el = state.element;
+      var obj = el.children('ul');
+      
+      var clone = obj.children('li:last').clone(true);
+      
+      if(options.height > 0)
+      {
+        height = obj.children('li:first').height();
+      }
+      
+      obj.css('top', '-' + height + 'px')
+        .prepend(clone);
+      
+      if(attribs && attribs.animate) {
+        if(state.animating) return;
+        state.animating = true;
+        obj.animate({top: 0}, options.speed, function() {
+          $(obj).children('li:last').remove();
+          state.animating = false;
+        });
+      } else {
+        obj.children('li:last').remove();
+        obj.css('top', '0px');
+      }
+    },
+
+    nextUsePause: function() {
+      var state = $(this).data('state');
+      var options = state.options;
+      if(state.isPaused) return;
+      methods.next.call( this, {animate:options.animate} );
+    }
+
+  };
+
+  var methods = {
+
+    init: function(options) {
+      var defaultsClone = jQuery.extend({}, defaults);
+      var options = $.extend(defaultsClone, options);
+      var el = $(this);
+      var state = { 
+        itemHeight: 0,
+        element: el,
+        animating: false,
+        options: options,
+        isPaused: false
+      };
+      $(this).data('state', state);
+
+      el.css({overflow: 'hidden', position: 'relative'})
+        .children('ul').css({position: 'absolute', margin: 0, padding: 0})
+        .children('li').css({margin: 0, padding: 0});
+
+      if(isNaN(options.height) || options.height == 0)
+      {
+        el.children('ul').children('li').each(function(){
+          var current = $(this);
+          if(current.height() > state.itemHeight)
+            state.itemHeight = current.height();
+        });
+
+        // set the same height on all child elements
+        el.children('ul').children('li').each(function(){
+          var current = $(this);
+          current.height(state.itemHeight);
+        });
+
+        // set element to total height
+        el.height(state.itemHeight * options.showItems);
+      }
+      else
+      {
+        // set the preferred height
+        el.height(options.height);
+      }
+
+      var initThis = this;
+      
+      var interval = setInterval(function(){ 
+        internal.nextUsePause.call( initThis );
+      }, options.pause);
+
+      if(options.mousePause)
+      {
+        el.bind("mouseenter",function(){
+          methods.pause.call( initThis, true );
+        }).bind("mouseleave",function(){
+          methods.pause.call( initThis, false );
+        });
+      }
+    },
+
+    pause: function(pauseState) {
+      var state = $(this).data('state');
+      state.isPaused = pauseState;
+    },
+
+    next: function(attribs) { 
+      var state = $(this).data('state');
+      if(state.animating) return false;
+      internal.moveUp(state, attribs); 
+    },
+
+    prev: function(attribs) {
+      var state = $(this).data('state');
+      if(state.animating) return false;
+      internal.moveDown(state, attribs); 
+    }
+  };
  
-	$.fn.vTicker = function( method ) {
-
-		if ( methods[method] ) {
-		  return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || ! method ) {
-		  return methods.init.apply( this, arguments );
-		} else {
-		  $.error( 'Method ' +  method + ' does not exist on jQuery.vTicker' );
-		}    
-	};
+  $.fn.vTicker = function( method ) {
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.vTicker' );
+    }    
+  };
 })(jQuery);
